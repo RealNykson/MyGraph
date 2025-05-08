392d4ac077e3c5be2ac213e218660f13652f2505
felix <felix@mast.com>
2025-05-08 23:04:56 +0200

    Added comments/Clean up

--- Views/Menu/Menu.xaml.cs
+++ Views/Menu/Menu.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyGraph.Views
+namespace MyGraph.Views.Menu
 {
   /// <summary>
   /// Interaction logic for Menu.xaml
